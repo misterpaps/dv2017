f7f1e228a6ad7b7ea388e79cc7f5101bc9eb8ab2
Paps <paps@prototypedesign.net>
2017-03-28 15:33:44 -0700

    Added Google Analytics link. For reals this time. The is initial launch version of the theme.

--- header.php
+++ header.php
@@ -22,6 +22,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php include_once("analyticstracking.php") ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'dvseventeen' ); ?></a>
 
